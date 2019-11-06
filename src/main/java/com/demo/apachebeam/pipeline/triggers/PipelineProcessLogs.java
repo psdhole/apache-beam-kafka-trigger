51b26b80d898f8b355371283ec2c873b90ac2e9b
Pushparaj Dhole <Pushparaj.Dhole@mobiliya.com>
2019-11-06 14:49:39 +0530

    added csv data

--- src/main/java/com/demo/apachebeam/pipeline/triggers/PipelineProcessLogs.java
+++ src/main/java/com/demo/apachebeam/pipeline/triggers/PipelineProcessLogs.java
@@ -3,6 +3,7 @@
  */
 package com.demo.apachebeam.pipeline.triggers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
@@ -13,11 +14,12 @@ import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.transforms.SimpleFunction;
 import org.apache.beam.sdk.transforms.windowing.*;
 import org.apache.beam.sdk.values.KV;
-import org.apache.beam.sdk.values.PCollection;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.joda.time.Duration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Map;
 
 /** The Class {@link PipelineProcessLogs}. */
 public class PipelineProcessLogs {
@@ -39,44 +41,56 @@ public class PipelineProcessLogs {
     final PipelineOptions options =
         PipelineOptionsFactory.fromArgs().withValidation().as(PipelineOptions.class);
     Pipeline pipeline = Pipeline.create(options);
-    PCollection<String> output =
-        pipeline
-            .apply(
-                KafkaIO.<String, String>read()
-                    .withBootstrapServers(BOOTSTRAP_SERVERS)
-                    .withTopic(INPUT_TOPIC)
-                    .withKeyDeserializer(StringDeserializer.class)
-                    .withValueDeserializer(StringDeserializer.class)
-                    .updateConsumerProperties(
-                        ImmutableMap.of("auto.offset.reset", (Object) "earliest"))
-                    .withoutMetadata())
-            .apply(
-                "Apply Fixed window: ",
-                Window.<KV<String, String>>into(FixedWindows.of(Duration.standardMinutes(2)))
-                    .triggering(
-                        Repeatedly.forever(
-                            AfterFirst.of(
-                                AfterPane.elementCountAtLeast(90),
-                                AfterProcessingTime.pastFirstElementInPane()
-                                    .plusDelayOf(Duration.standardMinutes(2)))))
-                    .withAllowedLateness(Duration.ZERO)
-                    .discardingFiredPanes())
-            .apply(
-                MapElements.via(
-                    new SimpleFunction<KV<String, String>, String>() {
-                      private static final long serialVersionUID = 1L;
-                      @Override
-                      public String apply(KV<String, String> inputJSON) {
-                        return inputJSON.getValue();
-                      }
-                    }));
-    output.apply(
-        TextIO.write()
-            .withWindowedWrites()
-            .withShardNameTemplate("-logfile-SS-of-NN")
-            .to(OUTPUT_FILE_NAME)
-            .withNumShards(5)
-            .withSuffix(".csv"));
+
+    pipeline
+        .apply(
+            KafkaIO.<String, String>read()
+                .withBootstrapServers(BOOTSTRAP_SERVERS)
+                .withTopic(INPUT_TOPIC)
+                .withKeyDeserializer(StringDeserializer.class)
+                .withValueDeserializer(StringDeserializer.class)
+                .updateConsumerProperties(ImmutableMap.of("auto.offset.reset", (Object) "earliest"))
+                .withoutMetadata())
+        .apply(
+            "Apply Fixed window: ",
+            Window.<KV<String, String>>into(FixedWindows.of(Duration.standardMinutes(2)))
+                .triggering(
+                    Repeatedly.forever(
+                        AfterFirst.of(
+                            AfterPane.elementCountAtLeast(90),
+                            AfterProcessingTime.pastFirstElementInPane()
+                                .plusDelayOf(Duration.standardMinutes(2)))))
+                .withAllowedLateness(Duration.ZERO)
+                .discardingFiredPanes())
+        .apply(
+            MapElements.via(
+                new SimpleFunction<KV<String, String>, String>() {
+                  private static final long serialVersionUID = 1L;
+
+                  @Override
+                  public String apply(KV<String, String> inputJSON) {
+                    String csvRow = "";
+                    try {
+                      ObjectMapper mapper = new ObjectMapper();
+                      Map<String, String> map = mapper.readValue(inputJSON.getValue(), Map.class);
+                      String logType = (String) map.get("logType");
+                      String logSeverity = (String) map.get("logSeverity");
+                      String logPriority = (String) map.get("logPriority");
+                      String logDescription = (String) map.get("logDescription");
+                      csvRow = String.join(",", logType, logPriority, logSeverity, logDescription);
+                    } catch (Exception e) {
+                      LOGGER.debug("Error while parsing JSON :", e);
+                    }
+                    return csvRow;
+                  }
+                }))
+        .apply(
+            TextIO.write()
+                .withWindowedWrites()
+                .withShardNameTemplate("-logfile-SS-of-NN")
+                .to(OUTPUT_FILE_NAME)
+                .withNumShards(5)
+                .withSuffix(".csv"));
     pipeline.run();
     LOGGER.debug("All done ..!!");
   }
